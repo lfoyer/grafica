b3e015cdc37da155ebb500a00e518e2b96732b7f
lfoyer <miker-la-marmotte@hotmail.fr>
2024-05-15 01:50:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'https://unpkg.com/three@0.128.0/examples/jsm/controls/OrbitControls.js';
 import { getTrain } from './train.js';
 import { getTunnel } from './tunnel.js';
 import { getTrackPath } from './track_path.js';
